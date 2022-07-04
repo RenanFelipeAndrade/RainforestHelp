08ab70d3b8a159f97931bb11fe0c01327f1d23fb
Renan Andrade <Fel9.renan02@gmail.com>
2022-07-04 14:22:53 -0300

    Adicionado animações suaves ao scroll do carousel

--- main.js
+++ main.js
@@ -70,3 +70,11 @@ setInterval(function() {
     counter = 1;
   }
 }, 5000);
+
+document.querySelector(".carousel").addEventListener("wheel", (event) => {
+  if (event.deltaY > 0) {
+    event.target.scrollBy(300, 0);
+  } else {
+    event.target.scrollBy(-300, 0);
+  }
+});
